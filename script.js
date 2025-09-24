497196d1948249f02c3d40362e3cffab210b5db6
osmanwael <osmanwael0887@gmail.com>
2025-09-24 14:13:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var typed = new Typed(".typing", {
-        strings: ["مدير مالي", "مدير حسابات", "رئيس حسابات"],
+        strings: ["مدير مالي", "مراجع حسابات", "رئيس حسابات"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true,
